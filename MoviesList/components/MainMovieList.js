912a699acebf084c56db78b1c4c7bf73ef5b51df
Shubhangi <shubhangi@teewe.in>
2017-04-27 15:08:47 +0530

    modified some code

--- MoviesList/components/MainMovieList.js
+++ MoviesList/components/MainMovieList.js
@@ -14,7 +14,9 @@ import DisplayMovieCards from '../components/DisplayMovieCards';
 export default class MainMoviesList extends Component{
 	constructor(props){
 		super(props);
+		const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 		this.state = {
+			moviesData: ds.cloneWithRows([]),
 			movieSearched: '',
 			movieDetails:{
 				originalTitle: '',
@@ -23,10 +25,38 @@ export default class MainMoviesList extends Component{
 		};
 	}
 
+	componentDidMount() {
+        this.fetchMoviesData();
+    }
+
+    renderRow(rowData){
+        return (
+            <View style={styles.card}>
+                <Image
+                    source={{uri:'https://image.tmdb.org/t/p/w500_and_h281_bestv2/'+rowData.poster_path}}
+                    resizeMode="contain"
+                    style={styles.img} />
+                <Text style={styles.txt}>{rowData.title} (Rating: {rowData.vote_average})</Text>
+            </View>
+        );
+    }
+
+    fetchMoviesData() {
+        const url = 'http://api.themoviedb.org/3/movie/now_playing?api_key=466839c4d8544152a58da0ad13d38545';
+        fetch(url)
+            .then( response => response.json() )
+            .then( jsonData => {
+                this.setState({
+                    moviesData: this.state.moviesData.cloneWithRows(jsonData.results),
+                });
+            })
+            .catch( error => console.log('Error fetching: ' + error) );
+    }
+
 	handleTextChange(event){
 		const movieSearched = event.nativeEvent.text
 		this.setState({
-			movieSearched: movieSearched
+			movieSearched: "You searched for " + movieSearched
 		});
 
 		// Fecth API to get data from the TMDB API 
@@ -50,9 +80,23 @@ export default class MainMoviesList extends Component{
 		return(
 				<View style={styles.container}>
 					<View>
-						<TextInput placeholder="Search Movies" style={styles.input} onSubmitEditing={this.handleTextChange.bind(this)}></TextInput>
-						<Text>You searched for {this.state.movieSearched}</Text>
+						<View style={styles.searchBar}>
+							<TextInput placeholder="Search Movies" style={styles.input} onSubmitEditing={this.handleTextChange.bind(this)}></TextInput>
+						</View>
+
+						<Text>{this.state.movieSearched}</Text>
+
 						<DisplayMovieCards originalTitle={this.state.movieDetails.originalTitle} overview={this.state.movieDetails.overview}/>
+						
+						<View style={styles.wrapper}>
+							<Text>Now Playing</Text>
+							<View style={styles.movieListCards}>
+								<ListView 
+									dataSource = {this.state.moviesData}
+									renderRow  = {this.renderRow}
+								/>
+							</View>
+						</View>
 					</View>
 				</View>
 			);
@@ -63,6 +107,7 @@ const {height, width} = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
 	container:{
+		marginTop: 30,
 		justifyContent: 'center',
 		alignItems: 'center'
 	},
@@ -72,8 +117,30 @@ const styles = StyleSheet.create({
 	movieCard:{
 
 	},
+	searchBar:{
+		backgroundColor: '#ddd'
+	},
 	input:{
 		width,
 		height: 50
-	}
+	},
+	wrapper:{
+		justifyContent: 'center',
+		alignItems: 'center'
+	},
+	card: {
+        borderWidth: 1,
+        borderColor: '#eee',
+        backgroundColor: '#fff',
+        marginBottom: 10,
+        elevation: 1
+    },
+    img: {
+        height: 200
+    },
+    txt: {
+        margin: 10,
+        fontSize: 16,
+        textAlign: 'left'
+    },
 });
